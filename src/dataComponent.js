68a2b40551464992f6a73c826e707b4f63649566
Martin Willitts <martin@martinwillitts.com>
2017-03-31 16:54:50 +0100

    Handle data value as number as well as string

--- src/dataComponent.js
+++ src/dataComponent.js
@@ -6,7 +6,8 @@ export default class DataComponent extends Component {
     if (typeof this.props.data != 'undefined' && this.props.data && this.props.data.length>0) {
       this.props.data.map(d => {
         if (d.name && d.value) {
-          extraProps["data-" + encodeURIComponent(d.name)] = d.value.replace(/'/g, '&quot;');
+          const v = typeof d.value == "string" ? d.value.replace(/'/g, '&quot;') : d.value;
+          extraProps["data-" + encodeURIComponent(d.name)] = v;
         }
       })
     }
